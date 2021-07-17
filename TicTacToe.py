f0cbde2687ef5d5d2cbc0eec4b15737709e5a2b9
mgaines11 <iammatthewgaines@gmail.com>
2021-07-16 20:06:38 -0500

    Create TicTacToe.py

---
+++ TicTacToe.py
@@ -0,0 +1,13 @@
+import pygame, sys
+
+pygame.init()
+
+WIDTH = 600
+HEIGHT = 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# mainloop
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit()
